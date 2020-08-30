67a493e3e7264ba57b89d57c22e5a1e0d964aeea
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2020-08-30 21:21:55 +0300

    add Task6

---
+++ src/homework01/Task6.java
@@ -0,0 +1,34 @@
+package homework01;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int[] phoneNumber = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String formattedPhoneNumber;
+
+        formattedPhoneNumber = createPhoneNumber(phoneNumber);
+        System.out.println(formattedPhoneNumber);
+    }
+
+
+    static String createPhoneNumber(int[] number) {
+        String formatNumber;
+        char[] arrNumber = new char[14];
+
+        for (int i = 0, n = 0; i < 14; i++) {
+            if (i == 0) {
+                arrNumber[i] = '(';
+            } else if (i == 4) {
+                arrNumber[i] = ')';
+
+                arrNumber[++i] = ' ';
+            } else if (i == 9) {
+                arrNumber[i] = '-';
+            } else {
+                arrNumber[i] = (char) ('0' + number[n]);
+                n++;
+            }
+        }
+        formatNumber = new String(arrNumber);
+        return (formatNumber);
+    }
+}
