b0bf8c3cfa1c5801e73dc63cfe6e582d06d7198b
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2020-09-06 15:18:02 +0300

    add Task1_4

---
+++ src/homework2/Task1_4.java
@@ -0,0 +1,15 @@
+package homework2;
+
+public class Task1_4 {
+    public static void main(String[] args) {
+        long a = 1;
+        long aPrevious = 0;
+        long factor = 3;
+        int i = 0;
+        while (a >= 0) {
+            aPrevious = a;
+            a *= factor;
+        }
+        System.out.println("Long before overflow = " + aPrevious + " and after overflow = " + a);
+    }
+}
