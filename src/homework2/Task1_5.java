12703bfb8db697de7d784c62d967a44982da9e7f
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2020-09-06 18:32:17 +0300

    refactor  Task1_5

--- src/homework2/Task1_5.java
+++ src/homework2/Task1_5.java
@@ -2,33 +2,31 @@ package homework2;
 
 public class Task1_5 {
     public static void main(String[] args) {
-        int startNumberInFirstColumn = 2;
-        int endNumberInLAstColumn = 9;
+        int startNumber = 2;
+        int endNumber = 9;
         int columnsInRow = 4;
         int firstNumeralToMult = 1;
         int lastNumeralToMult = 9;
         int rows = 0;
+        int startNumberInRow;
+        int endNumberInRow;
 
-        if (startNumberInFirstColumn > endNumberInLAstColumn ||
-                firstNumeralToMult > lastNumeralToMult) {
+        if (startNumber > endNumber
+                || firstNumeralToMult > lastNumeralToMult) {
             System.out.println("Bad data");
             return;
         }
         System.out.println();
-        System.out.println( "\t \t \t Таблица умножения");
-        rows = (int) Math.ceil((double) (endNumberInLAstColumn -
-                startNumberInFirstColumn + 1) / columnsInRow);
+        System.out.println("\t \t \t Таблица умножения");
+        rows = (int) Math.ceil((double) (endNumber -
+                startNumber + 1) / columnsInRow);
         for (int i = 0, j = 1; i < rows; i++, j++) {
-            int start = startNumberInFirstColumn + (columnsInRow * i);
-            int end = startNumberInFirstColumn - 1 + (columnsInRow * j);
-            if ((startNumberInFirstColumn - 1 + (columnsInRow * j) <
-                    endNumberInLAstColumn))
-            {
-                columns(start, end, firstNumeralToMult, lastNumeralToMult);
-            } else {
-                columns(start, endNumberInLAstColumn, firstNumeralToMult,
-                        lastNumeralToMult);
-            }
+            startNumberInRow = startNumber + (columnsInRow * i);
+            endNumberInRow = Math.min(startNumber - 1 + (columnsInRow * j),
+                    endNumber);
+
+            columns(startNumberInRow, endNumberInRow, firstNumeralToMult,
+                    lastNumeralToMult);
             System.out.println();
         }
 
@@ -36,13 +34,13 @@ public class Task1_5 {
     }
 
     // method of columns construction
-    static void columns(int startNumber, int endNumber, int firstMultiplier,
-                        int lastMultiplier) {
+    static void columns(int startNumberInRow, int endNumberInRow,
+                        int firstMultiplier, int lastMultiplier) {
         for (int i = firstMultiplier; i <= lastMultiplier; i++) {
-            for (int j = startNumber; j <= endNumber; j++) {
+            for (int j = startNumberInRow; j <= endNumberInRow; j++) {
                 System.out.print(j + " x " + i + " = " + (j * i) + '\t');
             }
             System.out.println();
-         }
+        }
     }
 }
