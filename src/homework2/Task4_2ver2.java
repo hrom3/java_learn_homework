312f5a61f90e039508140a568140492d8715d72c
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2020-09-06 23:19:06 +0300

    add Task4_2ver2

---
+++ src/homework2/Task4_2ver2.java
@@ -0,0 +1,101 @@
+package homework2;
+
+import java.util.Arrays;
+
+
+public class Task4_2ver2 {
+
+
+    static void toSwap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+
+    /**
+     * Bubble sort of array
+     *
+     * @param array to sort
+     * @return new sorted array
+     */
+    static int[] bubbleSorter(int[] array) {
+        int[] sortArray = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < sortArray.length; i++) {
+            for (int j = sortArray.length - 1; j > i; j--) {
+                if (sortArray[j] < sortArray[j - 1]) {
+                    toSwap(sortArray, (j - 1), j);
+                }
+            }
+        }
+        return sortArray;
+    }
+
+    /**
+     * Shaker sorting
+     *
+     * @param array to sort
+     * @return new sorted array
+     */
+    static int[] shakerSorter(int[] array) {
+        int[] sortArray = Arrays.copyOf(array, array.length);
+        int left = 0;
+        int right = sortArray.length - 1;
+
+        do {
+            for (int i = left + 1; i <= right; i++) {
+                if (sortArray[i] < sortArray[i - 1]) {
+                    toSwap(sortArray, (i - 1), i);
+                }
+            }
+            right--;
+            for (int i = right; i > left; i--) {
+                if (sortArray[i] < sortArray[i - 1]) {
+                    toSwap(sortArray, (i - 1), i);
+                }
+            }
+            left++;
+        } while (left < right);
+        return sortArray;
+    }
+
+
+    public static void main(String[] args) {
+        int[] testArr1 = {1, 2, 3, 4, 5, 6};
+        int[] testArr2 = {1, 1, 1, 1};
+        int[] testArr3 = {9, 1, 5, 99, 9, 9};
+        int[] testArr4 = {};
+        int[] testArr5 = {4, 8, 33, -5, -965, 0};
+
+        int[] sortTestArr1 = bubbleSorter(testArr1);
+//        System.out.println(Arrays.toString(testArr1) + " -> " + Arrays.toString(sortTestArr1) + '\n');
+
+        int[] sortTestArr2 = bubbleSorter(testArr2);
+//        System.out.println(Arrays.toString(testArr2) + " -> " + Arrays.toString(sortTestArr2) + '\n');
+
+        int[] sortTestArr3 = bubbleSorter(testArr3);
+//        System.out.println(Arrays.toString(testArr3) + " -> " + Arrays.toString(sortTestArr3) + '\n');
+
+        int[] sortTestArr4 = bubbleSorter(testArr4);
+//        System.out.println(Arrays.toString(testArr4) + " -> " + Arrays.toString(sortTestArr4) + '\n');
+
+        int[] sortTestArr5 = bubbleSorter(testArr5);
+//        System.out.println(Arrays.toString(testArr5) + " -> " + Arrays.toString(sortTestArr5) + '\n');
+
+
+        int[] sortTestArr1S = shakerSorter(testArr1);
+        System.out.println(Arrays.toString(testArr1) + " -> " + Arrays.toString(sortTestArr1) + " -> " + Arrays.toString(sortTestArr1S) + '\n');
+
+        int[] sortTestArr2S = shakerSorter(testArr2);
+        System.out.println(Arrays.toString(testArr2) + " -> " + Arrays.toString(sortTestArr2) + " -> " + Arrays.toString(sortTestArr2S) + '\n');
+
+        int[] sortTestArr3S = shakerSorter(testArr3);
+        System.out.println(Arrays.toString(testArr3) + " -> " + Arrays.toString(sortTestArr3) + " -> " + Arrays.toString(sortTestArr3S) + '\n');
+
+        int[] sortTestArr4S = shakerSorter(testArr4);
+        System.out.println(Arrays.toString(testArr4) + " -> " + Arrays.toString(sortTestArr4) + " -> " + Arrays.toString(sortTestArr4S) + '\n');
+
+        int[] sortTestArr5S = shakerSorter(testArr5);
+        System.out.println(Arrays.toString(testArr5) + " -> " + Arrays.toString(sortTestArr5) + " -> " + Arrays.toString(sortTestArr5S) + '\n');
+    }
+}
