0b89bd63514a47e13e0c25604fe8b44b07be30ed
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2020-09-07 22:08:20 +0300

    edit Task4

--- src/homework2/Task4.java
+++ src/homework2/Task4.java
@@ -8,6 +8,8 @@ import java.util.Arrays;
 
 public class Task4 {
     public static void main(String[] args) {
+
+        // Task 4.4
         int[] arrOfNumber;
         int[] arrOfNumberBubbleSort;
         int[] arrOfNumberShakerSort;
@@ -15,7 +17,8 @@ public class Task4 {
         Task4_4 getArr = new Task4_4();
         arrOfNumber = getArr.createRandomArray();
 
-        System.out.println("Random array\t\t  -> \t" + Arrays.toString(arrOfNumber));
+        System.out.println("Random array\t\t  -> \t" +
+                Arrays.toString(arrOfNumber));
 
         arrOfNumberBubbleSort = Task4_2ver3.bubbleSorter(arrOfNumber);
         arrOfNumberShakerSort = Task4_2ver3.shakerSorter(arrOfNumber);
@@ -24,6 +27,27 @@ public class Task4 {
                 Arrays.toString(arrOfNumberBubbleSort));
         System.out.println("Sorted Array (shaker) -> \t" +
                 Arrays.toString(arrOfNumberShakerSort));
+        System.out.println();
+
+        // Task 4.5
+        int arrLengthFromConsole;
+        int[] arrFromConsole;
+        int[] arrFromConsoleBubbleSort;
+        int[] arrFromConsoleShakerSort;
+
+        arrLengthFromConsole = Task2_1.getArrayLength();
+        arrFromConsole = Task2_1.getNumbersOfArray(arrLengthFromConsole);
+
+        System.out.println("Array from console\t  -> \t" +
+                Arrays.toString(arrFromConsole));
+
+        arrFromConsoleBubbleSort = Task4_2ver3.bubbleSorter(arrFromConsole);
+        arrFromConsoleShakerSort = Task4_2ver3.bubbleSorter(arrFromConsole);
+
+        System.out.println("Sorted Array (bubble) -> \t" +
+                Arrays.toString(arrFromConsoleBubbleSort));
+        System.out.println("Sorted Array (shaker) -> \t" +
+                Arrays.toString(arrFromConsoleShakerSort));
     }
 
 }
