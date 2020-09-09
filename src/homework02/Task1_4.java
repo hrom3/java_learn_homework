640a9d00aba822b365093e7e82998d4e61efea7e
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2020-09-09 21:50:30 +0300

    change Task1_4.java

--- src/homework02/Task1_4.java
+++ src/homework02/Task1_4.java
@@ -5,11 +5,32 @@ public class Task1_4 {
         long a = 1;
         long aPrevious = 0;
         long factor = 3;
-        int i = 0;
-        while (a >= 0) {
+        boolean isOverflow = false;
+        while (!isOverflow) {
             aPrevious = a;
-            a *= factor;
+            for (int i = 1; i < factor ; i++) {
+                if (willAdditionOverflow(aPrevious, a)) {
+                    isOverflow = true;
+                }
+                a += aPrevious;
+            }
         }
         System.out.println("Long before overflow = " + aPrevious + " and after overflow = " + a);
+    }
+
+    public static boolean willAdditionOverflow(long left, long right) {
+        if (right < 0 && right != Long.MIN_VALUE) {
+            return willSubtractionOverflow(left, -right);
+        } else {
+            return (~(left ^ right) & (left ^ (left + right))) < 0;
+        }
+    }
+
+    public static boolean willSubtractionOverflow(long left, long right) {
+        if (right < 0) {
+            return willAdditionOverflow(left, -right);
+        } else {
+            return ((left ^ right) & (left ^ (left - right))) < 0;
+        }
     }
 }
