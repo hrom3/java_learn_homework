af9943b14e21409829d9d2cccb8351c517c4812e
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2020-10-05 23:38:39 +0300

    update RandomMy

--- src/utils/RandomMy.java
+++ src/utils/RandomMy.java
@@ -1,6 +1,8 @@
 package utils;
 
+import homework05.ReadFileToString;
 import java.util.Random;
+
 
 public class RandomMy {
     final static private char[] allString = ("QWERTYUIOPASDFGHJKLZXCVBNM" +
@@ -15,5 +17,33 @@ public class RandomMy {
             b.append(allString[rnd.nextInt(allString.length)]);
         }
         return  b.toString();
+    }
+
+    public static String randomNextNick() {
+        String filePath =
+                "d:\\repository\\java_learn\\HomeTasks\\pet_niks.txt";
+        String data = ReadFileToString.readAllBytes(filePath);
+
+        // Получение массива слов из строки
+        String[] arrayOfWords = data.split("\\s*(\\s|,|!|\\?|;|:|\\(|" +
+                "(\\)\\.*\\s*)|\\.+|\\*|(\\s*\"\\.*\\s*))\\s*");
+
+        int lengthOfArray = arrayOfWords.length;
+        String b = arrayOfWords[rnd.nextInt(lengthOfArray)];
+        return  b;
+    }
+
+    public static String randomNextName() {
+        String filePath =
+                "d:\\repository\\java_learn\\HomeTasks\\names.txt";
+        String data = ReadFileToString.readAllBytes(filePath);
+
+        // Получение массива слов из строки
+        String[] arrayOfWords = data.split("\\s*(\\s|,|!|\\?|;|:|\\(|" +
+                "(\\)\\.*\\s*)|\\.+|\\*|(\\s*\"\\.*\\s*))\\s*");
+
+        int lengthOfArray = arrayOfWords.length;
+        String b = arrayOfWords[rnd.nextInt(lengthOfArray)];
+        return  b;
     }
 }
