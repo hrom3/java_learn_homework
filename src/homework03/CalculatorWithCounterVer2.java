a6a4ea91d7ce1d42c1b506ed339a2f927517ebeb
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2020-10-10 20:25:04 +0300

    update homework 03

---
+++ src/homework03/CalculatorWithCounterVer2.java
@@ -0,0 +1,69 @@
+package homework03;
+
+/*
+ *  Task 8*. Создать класс CalculatorWithCounter, все методы в данном классе
+ * НЕ статические. У данного класса должно быть три конструктора (или один
+ * если сможете, это ещё не изучено
+ * https://refactoring.guru/ru/design-patterns/decorator) один принимающий
+ * в себя CalculatorWithOperator, второй CalculatorWithMathCopy, третий
+ * CalculatorWithMathExtends. Данные конструкторы должны сохранять переданные
+ * калькуляторы в приватные поля класса для дальнейшего их использования. Он
+ * также имеет 7 методов как и другие калькуляторы + 1 метод getCountOperation()
+ * который должен вернуть значение внутреннего счётчика.
+ *  В реализации методов должен быть вызван один из объектов переданный в
+ * констуктор при создании объекта CalculatorWithCounter, и вызвать
+ * соответсвующий метод у них. К примеру. Если вызывают метод plus у
+ * CalculatorWithCounter то он должен вызвать метод plus у объекта
+ * CalculatorWithOperator или у объекта CalculatorWithMathCopy или у объекта
+ * CalculatorWithMathExtends (смотря что передали в конструктор) и увеличить
+ * значение внутреннего счётчика операций.
+ */
+
+public class CalculatorWithCounterVer2 implements ICalculator {
+    private final ICalculator calcType;
+    private int countOperation = 0;
+
+
+    public CalculatorWithCounterVer2(ICalculator calcType) {
+        this.calcType = calcType;
+    }
+
+    public int getCountOperation() {
+        return countOperation;
+    }
+
+    public double add(double first, double second) {
+        countOperation++;
+        return calcType.add(first, second);
+    }
+
+    public double subtr(double first, double second) {
+        countOperation++;
+        return calcType.subtr(first, second);
+    }
+
+    public double multipl(double first, double second) {
+        countOperation++;
+        return calcType.multipl(first, second);
+    }
+
+    public double division(double first, double second) {
+        countOperation++;
+        return calcType.division(first, second);
+    }
+
+    public double power(double base, int degree) {
+        countOperation++;
+        return calcType.power(base, degree);
+    }
+
+    public double abs(double number) {
+        countOperation++;
+        return calcType.abs(number);
+    }
+
+    public double sqrt(double number) {
+        countOperation++;
+        return calcType.sqrt(number);
+    }
+}
