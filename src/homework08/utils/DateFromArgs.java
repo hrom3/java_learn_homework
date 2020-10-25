5a7d56c574a2ba260ff6c5a2abc4191d3ff04f70
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2020-10-25 23:13:07 +0300

    homework 08 part 3

---
+++ src/homework08/utils/DateFromArgs.java
@@ -0,0 +1,125 @@
+package homework08.utils;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TreeSet;
+
+public class DateFromArgs {
+
+    private final static SimpleDateFormat stringFromDate = new SimpleDateFormat("yyyy-MM-dd");
+    private final static SimpleDateFormat dateFromString = new SimpleDateFormat("dd.MM.yyyy");
+
+    /**
+     * Возвращает TreeSet со значениями тип Date
+     *
+     * @param args Массив строк для обработки
+     * @return TreeSet со значениями тип Date в
+     */
+    protected static TreeSet<Date> dateArrListFromDate(String[] args) {
+        TreeSet<Date> listOfDate = new TreeSet<>();
+
+        if (args == null || args.length == 0) {
+            listOfDate.add(new Date());
+            return listOfDate;
+        }
+        if (args.length > 1) {
+            System.out.println("The program is launched with more than 1 argument. " +
+                    "Only the first argument will be used");
+        }
+
+        String delimiterDash = "-";
+        String delimiterComma = ",";
+        String strWithDate = args[0];
+        boolean isDelimiterDash;
+        boolean isDelimiterComma;
+        int indexOfDelimiter;
+        isDelimiterDash = strWithDate.contains(delimiterDash);
+
+        isDelimiterComma = strWithDate.contains(delimiterComma);
+
+        if (!isDelimiterComma && !isDelimiterDash) {
+            try {
+                listOfDate.add(strToDate(strWithDate));
+                return listOfDate;
+            } catch (ParseException e) {
+                System.out.println("Bad date argument.");
+                listOfDate.add(new Date());
+                return listOfDate;
+            }
+        }
+            // Заполняем list значениями типа 20.01.2016-20.02.2016
+        if (!isDelimiterComma && isDelimiterDash) {
+            indexOfDelimiter = strWithDate.indexOf(delimiterDash);
+            String dateStart = strWithDate.substring(0, indexOfDelimiter);
+            String dateEnd = strWithDate.substring(indexOfDelimiter + 1);
+            try {
+                Date date2 = strToDate(dateEnd);
+                Date date1 = strToDate(dateStart);
+
+                if (date1.equals(date2)) {
+                    listOfDate.add(date1);
+                    return listOfDate;
+                }
+                if (date1.compareTo(date2) < 0) {
+                    Calendar start = Calendar.getInstance();
+                    Calendar end = Calendar.getInstance();
+                    start.setTime(date1);
+                    end.setTime(date2);
+                    listOfDate.add(date1);
+                    while (!start.equals(end)) {
+                        start.add(Calendar.DATE, 1);
+                        listOfDate.add(start.getTime());
+                    }
+                    return listOfDate;
+                }
+                System.out.println("Bad date argument.");
+                listOfDate.add(new Date());
+                return listOfDate;
+            } catch (ParseException e) {
+                System.out.println("Bad date argument.");
+                listOfDate.add(new Date());
+                return listOfDate;
+            }
+        }
+            // Заполняем list значениями типа 20.01.2016,22.01.2016
+        if (isDelimiterComma && !isDelimiterDash) {
+            int indexStart = 0;
+            while (strWithDate.contains(delimiterComma)) {
+                String substr = strWithDate.substring(indexStart,
+                        strWithDate.indexOf(delimiterComma));
+                try {
+                    listOfDate.add(strToDate(substr));
+                    strWithDate = strWithDate.substring(strWithDate.indexOf(delimiterComma) + 1);
+                     if (!strWithDate.contains(delimiterComma)) {
+                        listOfDate.add(strToDate(strWithDate));
+                     }
+                } catch (ParseException e) {
+                    System.out.println("Bad date argument.");
+                    listOfDate.add(new Date());
+                    return listOfDate;
+                }
+            }
+            return listOfDate;
+        }
+        System.out.println("Bad date argument.");
+        listOfDate.add(new Date());
+        return listOfDate;
+    }
+
+    private static Date strToDate(String str) throws ParseException {
+        return dateFromString.parse(str);
+    }
+
+    private static String dateToStr(Date date) {
+        return stringFromDate.format(date);
+    }
+    public static void main(String[] args) {
+        TreeSet<Date> dateTreeSet = dateArrListFromDate(args);
+        for (Date date : dateTreeSet) {
+            System.out.println(dateToStr(date));
+        }
+    }
+}
